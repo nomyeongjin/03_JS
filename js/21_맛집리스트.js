3897f4f30eccd4d1b88d86b0dc31712957b35d08
nomyeongjin <bbagifa2555@gmail.com>
2024-01-22 17:24:49 +0900

    240122_window내장객체+스톱워치

---
+++ js/21_맛집리스트.js
@@ -0,0 +1,105 @@
+/* Resturant 생성자 함수 */
+function Restaurant(resName, resAddress, resMenu){
+    // 생성자 함수 안에서 this == 만들어지는 객체 자체
+    
+    //속성 (변수, 필드)
+    this.resName = resName;
+    this.resAddress = resAddress;
+
+    // 메뉴 ('메뉴1, 메뉴2, 메뉴3')
+    this.resMenu = resMenu.split(", "); //배열로 바꿔서 저장
+
+    // 기능(메서드)
+    this.information = function(){
+        return `${this.resName} / [${this.resMenu}] / ${this.resAddress}` ;
+    }
+}
+
+/* 문자열.split("구분자") */
+// -> 문자열을 "구분자"로 쪼개서 배열로 반환
+
+
+/* 전역변수로 식당 목록을 저장할 배열을 선언 */
+const resList = [];
+// 샘플 식당 데이터 추가
+resList.push(new Restaurant("김밥천국", "김밥, 라면","서울시 중구"));
+resList.push(new Restaurant("왕가탕후루", "샤인머스켓, 딸기, 통귤","서울시 서대문구"));
+
+
+/* 화면에 존재하는 요소들 얻어오시 */
+const resName = document.querySelector("#resName");
+const resAddress = document.querySelector("#resAddress")
+const resMenu = document.querySelector("#resMenu")
+const addBtn = document.querySelector("#addBtn")
+const selectBtn = document.querySelector("#selectBtn")
+
+/* addBtn 클릭 시 input에 입력된 값을 모두 얻어와 resList에 추가 */
+
+
+addBtn.addEventListener("click",()=>{
+
+    /* 유효성 검사 */
+    // -> 전달된 데이터가 형식, 크기, 작성법 등이 맞는지 검사
+    // ex) 입력된 값이 없을 경우
+    //      비밀 번호 형식이 맞지 않을 경우
+    //      입력된 값의 범위가 옳지 않을 경우
+
+    // 빈칸 검사
+    if(resName.value.trim().length==0){
+        alert("식당명을 입력해주세요.")
+        resName.focus();
+        return; // 함수를 종료시켜 아래 구문 수행 X
+
+    }
+    if(resAddress.value.trim().length==0){
+        alert("주소를 입력해주세요.")
+        resAddress.focus();
+        return; // 함수를 종료시켜 아래 구문 수행 X
+
+    }
+    if(resMenu.value.trim().length==0){
+        alert("메뉴를 입력해주세요.")
+        resMenu.focus();
+        return; // 함수를 종료시켜 아래 구문 수행 X
+
+    }
+
+
+    // resList에 식당객체 추가
+    resList.push(new Restaurant(resName.value, resAddress.value, resMenu.value))
+
+   
+    // 이전 input에 작성된 내용 삭제
+    resName.value="";
+    resAddress.value="";
+    resMenu.value ="";
+    alert("리스트에 추가되었습니다.");
+    
+
+    // 가게명 input 요소에 focus 맞추기
+    resName.focus();
+
+})
+
+/* 조회버튼 클릭시 #printList의 이전 내용을 모두 삭제하고
+resList의 요소를 이용해 li 요소를 만들어 추가하는 코드 */
+const printList = document.querySelector("#printList")
+
+selectBtn.addEventListener("click",()=>{
+
+
+    printList.innerHTML="";
+    for(i=0;i<resList.length;i++){
+        
+
+        const li = document.createElement("li");
+
+        //만들어진 li의 내용으로 resList[i]의 information() 메서드 반환값 대입
+        li.innerHTML=resList[i].information();
+    
+        // li 요소를 #printList의 마지막 자식으로 추가
+        printList.append(li);
+    }
+    
+
+})
